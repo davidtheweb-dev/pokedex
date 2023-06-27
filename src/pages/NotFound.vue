febcc5528b0d9652d0a5588d8a1d858eab8d54d3
davidheweb <davidhermosilladuro@gmail.com>
2023-06-27 22:21:23 +0200

    Changed home link from /pokemon to /

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -3,7 +3,7 @@
     <h2>¡Ups! Página no encontrada</h2>
     <p>
       Lo sentimos, pero la página que buscas no existe. Prueba a mirar la lista de
-      <router-link to="/pokemon">Pokemon</router-link>.
+      <router-link to="/">Pokemon</router-link>.
     </p>
   </section>
 </template>
