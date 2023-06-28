90bf3ea6b4e407b2b9a4e9e4f12a58dc4c8926e9
davidheweb <davidhermosilladuro@gmail.com>
2023-06-28 10:08:34 +0200

    Added PokemonItem

--- src/components/PokemonItem.vue
+++ src/components/PokemonItem.vue
@@ -1,141 +1,261 @@
+<template>
+  <div :class="['pokemon', { pokemon__captured: pokemon.captured }]">
+    <button v-if="isLogged" class="delete-pokemon" @click="deletePokemon(pokemon.id)">
+      <img src="./../assets/delete-button.svg" alt="Delete pokemon" />
+    </button>
+    <div class="pokemon__image" @click="goPokemonDetail">
+      <img :src="pokemon.sprite" :alt="pokemon.name" />
+    </div>
+    <div class="pokemon__info" @click="goPokemonDetail">
+      <h2 class="pokemon__name">{{ pokemon.name }}</h2>
+
+      <p v-if="pokemon.description" class="pokemon__description">
+        {{ pokemon.description }}
+      </p>
+
+      <div class="pokemon__types">
+        <span
+          v-for="type in pokemon.types"
+          :key="type"
+          class="pokemon__type"
+          :class="type.toLowerCase()"
+          >{{ type + ' ' }}</span
+        >
+      </div>
+
+      <div class="pokemon__abilities">
+        <span v-for="ability in pokemon.abilities" :key="ability" class="pokemon__ability">{{
+          ability + ' '
+        }}</span>
+      </div>
+
+      <div class="pokemon__stats">
+        <div class="pokemon__stat">
+          <div class="pokemon__stat-name">Height</div>
+          <div class="pokemon__stat-value">{{ pokemon.height }}</div>
+        </div>
+        <div class="pokemon__stat">
+          <div class="pokemon__stat-name">Weight</div>
+          <div class="pokemon__stat-value">{{ pokemon.weight }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup>
-defineProps({
-  book: {
+import { computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { useUserStore } from '../stores/user/UserStore';
+
+const userStore = useUserStore();
+const router = useRouter();
+
+const props = defineProps({
+  pokemon: {
     type: Object,
     required: true,
   },
 });
 
-const emit = defineEmits(['delete-book']);
+const emit = defineEmits(['delete-pokemon']);
 
-function deleteBook(id) {
-  emit('delete-book', id);
+const isLogged = computed(() => {
+  return userStore.isLogged;
+});
+
+function goPokemonDetail() {
+  if (isLogged.value) {
+    router.replace(`/${props.pokemon.id}`);
+  } else {
+    alert('You must log in to access the detailed information of the pokemon');
+  }
+}
+
+function deletePokemon(id) {
+  emit('delete-pokemon', id);
 }
 </script>
 
-<template>
-  <div :class="['book-item', { 'book-item--favourite': book.favourite }]">
-    <button class="book-item__delete" @click="deleteBook(book.id)">
-      <img src="./../assets/delete-button.svg" alt="Delete book" />
-    </button>
-    <div class="book-item__cover">
-      <img :src="book.cover" :alt="book.title" />
-    </div>
-    <div class="book-item__info">
-      <h2 class="book-item__title">{{ book.title }}</h2>
-      <p class="book-item__author">{{ book.author }}</p>
-      <div class="book-item__data">
-        <p class="book-item__year"><strong>Year: </strong>{{ book.year }}</p>
-        <p class="book-item__pages"><strong>Pages: </strong>{{ book.pages }}</p>
-        <p class="book-item__genre">
-          <strong>Genre: </strong
-          ><span v-for="genre in book.genre" :key="genre">{{ genre + ' ' }}</span>
-        </p>
-        <p class="book-item__editorial"><strong>Editorial: </strong>{{ book.editorial }}</p>
-        <p class="book-item__language"><strong>Language: </strong>{{ book.language }}</p>
-        <p class="book-item__isbn"><strong>ISBN: </strong>{{ book.isbn }}</p>
-      </div>
-    </div>
-
-    <p v-if="book.description" class="book-item__description">
-      {{ book.description }}
-    </p>
-
-    <p class="book-item__personal-note">{{ book.personalNote }}</p>
-
-    <div class="book-item__extra">
-      <p class="book-item__price"><strong>Price: </strong>{{ book.price }}</p>
-      <p class="book-item__stock"><strong>Stock: </strong>{{ book.stock }}</p>
-      <p class="book-item__rating">{{ book.rating }}</p>
-    </div>
-  </div>
-</template>
-
 <style scoped>
-.book-item {
+.pokemon {
   display: flex;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  overflow: hidden;
-  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 20px;
+  max-width: 400px;
+  padding: 1rem;
+  border-radius: 1rem;
+  background-color: #fff;
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease;
   position: relative;
 }
-.book-item--favourite {
-  background-color: #f5f5f5;
+.pokemon__captured {
+  background-color: #f0f0f0;
 }
-.book-item__delete {
-  background-color: transparent;
-  border: none;
-  padding: 10px;
-  cursor: pointer;
-  position: absolute;
-  top: 0;
-  right: 0;
-}
-.book-item__delete img {
-  width: 20px;
-}
-.book-item__cover {
-  width: 30%;
-  height: 300px;
-  overflow: hidden;
-  flex: 0 0 auto;
-}
-.book-item__cover img {
+.pokemon__image {
   width: 100%;
+  max-width: 200px;
+  height: 100%;
+  max-height: 200px;
+  margin-bottom: 1rem;
 }
-.book-item__info {
+.pokemon__image img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+.pokemon__info {
+  width: 100%;
   display: flex;
-  width: 70%;
   flex-direction: column;
-}
-.book-item__title {
-  font-size: 24px;
-  margin: 0;
-  padding: 20px;
-}
-.book-item__author {
-  font-size: 18px;
-  margin: 0;
-  padding: 0 20px;
-}
-.book-item__data {
-  display: flex;
-  flex-wrap: wrap;
-  padding: 0 20px;
-  border-top: 1px solid #ccc;
-  margin-top: 20px;
-}
-.book-item__data > p {
-  width: 50%;
-  margin: 10px 0;
-}
-.book-item__description {
-  font-size: 14px;
-  margin: 0;
-  /* background-color: #f5f5f5; */
-  padding: 10px 20px;
-  border-radius: 5px;
-}
-.book-item__personal-note {
-  width: 100%;
-  font-size: 14px;
-  margin-top: 10px;
-  background-color: #f5f5f5;
-  padding: 10px 20px;
-  margin: 0;
-  display: flex;
   align-items: center;
   justify-content: center;
 }
-.book-item__extra {
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-  align-items: center;
+.pokemon__name {
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin-bottom: 0.5rem;
+  color: #333;
+  text-decoration: none;
 }
-.book-item__extra > p {
-  width: 33%;
-  margin: 10px 0;
+.pokemon__description {
+  font-size: 1rem;
+  font-weight: 400;
+  margin-bottom: 1rem;
+}
+.pokemon__types {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 1rem;
+}
+.pokemon__type {
+  font-size: 0.75rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.25rem;
+  margin-right: 0.5rem;
+}
+.pokemon__type.fire {
+  background-color: #f08030;
+  color: #fff;
+}
+.pokemon__type.water {
+  background-color: #6890f0;
+  color: #fff;
+}
+.pokemon__type.grass {
+  background-color: #78c850;
+  color: #fff;
+}
+.pokemon__type.electric {
+  background-color: #f8d030;
+  color: #fff;
+}
+.pokemon__type.ice {
+  background-color: #98d8d8;
+  color: #fff;
+}
+.pokemon__type.fighting {
+  background-color: #c03028;
+  color: #fff;
+}
+.pokemon__type.poison {
+  background-color: #a040a0;
+  color: #fff;
+}
+.pokemon__type.ground {
+  background-color: #e0c068;
+  color: #fff;
+}
+.pokemon__type.flying {
+  background-color: #a890f0;
+  color: #fff;
+}
+.pokemon__type.psychic {
+  background-color: #f85888;
+  color: #fff;
+}
+.pokemon__type.bug {
+  background-color: #a8b820;
+  color: #fff;
+}
+.pokemon__type.rock {
+  background-color: #b8a038;
+  color: #fff;
+}
+.pokemon__type.ghost {
+  background-color: #705898;
+  color: #fff;
+}
+.pokemon__type.dragon {
+  background-color: #7038f8;
+  color: #fff;
+}
+.pokemon__type.dark {
+  background-color: #705848;
+  color: #fff;
+}
+.pokemon__type.steel {
+  background-color: #b8b8d0;
+  color: #fff;
+}
+.pokemon__type.fairy {
+  background-color: #ee99ac;
+  color: #fff;
+}
+.pokemon__abilities {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 1rem;
+}
+.pokemon__ability {
+  font-size: 0.75rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  padding: 0.25rem 0.5rem;
+  border-radius: 0.25rem;
+  margin-right: 0.5rem;
+  background-color: #f8f8f8;
+  color: #000;
+}
+.pokemon__stats {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.pokemon__stat {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.pokemon__stat-name {
+  font-size: 0.75rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-bottom: 0.25rem;
+}
+.pokemon__stat-value {
+  font-size: 1rem;
+  font-weight: 700;
+}
+.delete-pokemon {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: transparent;
+  border: 0;
+  padding: 0;
+  cursor: pointer;
+}
+.delete-pokemon img {
+  width: 20px;
 }
 </style>
